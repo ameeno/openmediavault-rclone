f5e88ed79f2d2915590326fb50dc35aabd034049
Ameeno <ameeno@users.noreply.github.com>
2022-03-01 10:31:52 +0000

    Added: output/.gitkeep, source/debian/.debhelper/generated/openmediavault-rclone/installed-by-dh_install, source/debian/.debhelper/generated/openmediavault-rclone/installed-by-dh_installdocs, source/debian/openmediavault-rclone.debhelper.log, source/debian/openmediavault-rclone/usr/share/doc/openmediavault-rclone/changelog.gz, source/debian/openmediavault-rclone/usr/share/doc/openmediavault-rclone/copyright, source/debian/openmediavault-rclone/usr/share/lintian/overrides/openmediavault-rclone, source/usr/share/openmediavault/confdb/create.d/conf.service.rclone.sh, source/usr/share/openmediavault/datamodels/conf.service.rclone.filesystem.json, source/usr/share/openmediavault/datamodels/conf.service.rclone.json, source/usr/share/openmediavault/datamodels/rpc.rclone.json, source/usr/share/openmediavault/engined/inc/59rclone.inc, source/usr/share/openmediavault/engined/rcp/rclone.inc, source/usr/share/openmediavault/engined/rpc/rclone.inc, source/usr/share/php/openmediavault/system/filesystem/backend/rclone.inc, source/usr/share/php/openmediavault/system/filesystem/rcloneimpl.inc, source/var/www/openmediavault/images/rclone.png, source/var/www/openmediavault/images/rclone.svg, source/var/www/openmediavault/js/omv/module/admin/storage/rclone/Mount.js, source/var/www/openmediavault/js/omv/module/admin/storage/rclone/Mounts.js, source/var/www/openmediavault/js/omv/module/admin/storage/rclone/Rclone.js Modified: .gitignore, build.sh Renamed: debian/changelog, debian/compat, debian/control, debian/copyright, debian/install, debian/lintian-overrides, usr/share/openmediavault/confdb/create.d/conf.service.rclone.sh, usr/share/openmediavault/datamodels/conf.service.rclone.filesystem.json, usr/share/openmediavault/datamodels/conf.service.rclone.json, usr/share/openmediavault/datamodels/rpc.rclone.json, usr/share/openmediavault/engined/inc/59rclone.inc, usr/share/openmediavault/engined/rcp/rclone.inc, usr/share/openmediavault/engined/rpc/rclone.inc, usr/share/php/openmediavault/system/filesystem/backend/rclone.inc, usr/share/php/openmediavault/system/filesystem/rcloneimpl.inc, var/www/openmediavault/images/rclone.png, var/www/openmediavault/images/rclone.svg, var/www/openmediavault/js/omv/module/admin/storage/rclone/Mount.js, var/www/openmediavault/js/omv/module/admin/storage/rclone/Mounts.js, var/www/openmediavault/js/omv/module/admin/storage/rclone/Rclone.js, debian/postinst, debian/postinst.ex, debian/postrm, debian/postrm.ex, debian/preinst.ex, debian/prerm.ex, debian/rules, debian/triggers

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-docker run --rm --tty --interactive \
-	--volume "`pwd`":/openmediavault-rclone \
-	--volume /Users/heiko/Downloads:/output \
-	--workdir /openmediavault-rclone \
-	ubuntu:bionic \
-	/bin/bash -c 'apt-get update && \
-		apt-get install -y dpkg-dev debhelper && \
-		find . -name ".DS_Store" -exec rm {} \; && \
-		dpkg-buildpackage -us -uc && \
-		mv /openmediavault-rclone_* /output'
+#! /usr/bin/env bash
+
+nerdctl run --rm --tty --interactive \
+  --volume "$(pwd)"/source:/source \
+  --volume "$(pwd)"/output:/output:rw \
+  ubuntu:bionic \
+  /bin/bash -c 'cp -r /source openmediavault-rclone && cd openmediavault-rclone && apt-get update && apt-get install -y dpkg-dev debhelper &&  find . -name ".DS_Store" -exec rm {} \; &&  dpkg-buildpackage -us -uc &&  cp -r ../openmediavault-rclone_* /output'
